3d8500a9b1bfaf4a8b016547f707f7ce9c90397b
Ian Gutwinski <iangutwinski@gmail.com>
2021-06-13 11:18:29 -0400

    add players dropdown

--- javascript-backend/app/controllers/games_controller.rb
+++ javascript-backend/app/controllers/games_controller.rb
@@ -1,2 +1,12 @@
 class GamesController < ApplicationController
+
+    def index 
+        games = Game.all
+        render json: games
+    end 
+
+    def create
+        game = Game.create(date: params[:date], time: params[:time], location: params[:location])
+    end 
+
 end
--- javascript-frontend/src/index.js
+++ javascript-frontend/src/index.js
@@ -1,3 +1,104 @@
-fetch("http://localhost:3000/players")
+//define our OOJS classes
+class Game{
+    constructor(date, time, location){
+        this.date = date;
+        this.time = time;
+        this.location = location;
+    }
+};
+
+class Player{
+    constructor(name, email){
+        this.name = name;
+        this.email = email; 
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    
+    //fetch games
+    fetch("http://localhost:3000/games")
     .then(r => r.json())
-    .then(r => console.log(r))
+    .then(r => console.log(r)
+    );
+    
+    //fetch players
+    fetch("http://localhost:3000/players")
+    .then(r => r.json())
+    .then(players => { 
+        
+        const playerArray = []
+        for (let p of players){
+            let x = new Player(p.name, p.email)
+            playerArray.push(x)
+        };
+        
+        //create a new game through form
+        function createGame(e) {
+            e.preventDefault()
+            
+            //define form fields
+            const newGameDate = document.getElementById('game-date')
+            const newGameTime = document.getElementById('game-time')
+            const newGameLocation = document.getElementById('game-location')
+            
+            //create new game object and save to db
+            let gameObj = new Game(newGameDate.value, newGameTime.value, newGameLocation.value)
+            
+            const configObj = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept":"application/json"
+                },
+                body: JSON.stringify(gameObj)
+            };
+            
+            fetch("http://localhost:3000/games", configObj)
+            
+            
+            //update the front end to show the change
+            let game = document.createElement('div')
+            game.className = "game-card"
+            let date = document.createElement('p')
+            date.innerText = `${gameObj.date}`
+            let time = document.createElement('p') 
+            time.innerText = `${gameObj.time}`
+            let location = document.createElement('p')
+            location.innerText = `${gameObj.location}`
+            game.appendChild(date)
+            game.appendChild(time)
+            game.appendChild(location)
+
+            //collection select on all the players
+        
+            let addPlayers = document.createElement('p')
+            addPlayers.innerText = "Add Players Using Dropdown:"
+            game.appendChild(addPlayers)
+            let dropDown = document.createElement('select')
+            dropDown.setAttribute('name','players')
+            for (let p of playerArray){
+                let option = document.createElement('option')
+                option.setAttribute('value', p.id )
+                option.innerText = p.name
+                dropDown.appendChild(option)
+            }
+
+
+
+            game.appendChild(dropDown)
+            let button = document.createElement('button')
+            button.innerText = "Add Player"
+            game.appendChild(button)
+
+            document.getElementById('canvas').appendChild(game)
+        };
+        
+        
+        document.getElementById('create-game-form').addEventListener("submit", createGame);
+        
+    });
+    
+
+});
